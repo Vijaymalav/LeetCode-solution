888d9e940b087b9b9dddd4d34a4b342f1f203bf2
Vijay malav <91518113+Vijaymalav@users.noreply.github.com>
2022-02-24 15:42:32 +0530

    Time: 97 ms (81.94%), Space: 30.3 MB (58.48%) - LeetHub

--- 148-sort-list/148-sort-list.cpp
+++ 148-sort-list/148-sort-list.cpp
@@ -10,21 +10,62 @@
  */
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
-        vector<int>vec;
-        ListNode* curr=head;
-       while(curr){
-           vec.push_back(curr->val);
-           curr=curr->next;
-           
-       }
-        sort(vec.begin(),vec.end());
-        curr=head;
-        int i=0;
-        while(curr){
-            curr->val=vec[i++];
-            curr=curr->next;
+    ListNode* findmid(ListNode* head){
+        ListNode* s=head;
+        ListNode* f=head->next;
+        while(f and f->next){
+            s=s->next;
+            f=f->next->next;
         }
-        return head;
+        ListNode* mid=s->next;
+        s->next=NULL;
+        return mid;
+    }
+    
+    ListNode* merge(ListNode* left,ListNode* right){
+        if(!left and !right)return NULL;
+        if(left==NULL)return right;
+        if(right==NULL)return left;
+        if(left->val>right->val){
+            right->next=merge(left,right->next);
+            return right;
+        }
+        else{
+            left->next=merge(left->next,right);
+            return left;
+        }
+    }
+    
+    
+    ListNode* mergesort(ListNode* head){
+        if(head->next==NULL)return head;
+        ListNode* mid=findmid(head);
+        ListNode* left=mergesort(head);
+        ListNode* right=mergesort(mid);
+        return merge(left,right);
+    } 
+    ListNode* sortList(ListNode* head) {
+        if(head==NULL)return NULL;
+        // Brueteforce approach
+//         vector<int>vec;
+//         ListNode* curr=head;
+//        while(curr){
+//            vec.push_back(curr->val);
+//            curr=curr->next;
+           
+//        }
+//         sort(vec.begin(),vec.end());
+//         curr=head;
+//         int i=0;
+//         while(curr){
+//             curr->val=vec[i++];
+//             curr=curr->next;
+//         }
+//         return head;
+        return mergesort(head);
+        
+        
+        
+        
     }
 };
