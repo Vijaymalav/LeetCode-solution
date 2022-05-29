4ab6697809a3a309c5e4c19cb1d450e8834c82b0
Vijay malav <91518113+Vijaymalav@users.noreply.github.com>
2022-05-29 19:51:19 +0530

    Time: 6 ms (54.82%), Space: 12.1 MB (37.77%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,36 +1,25 @@
 class Solution {
 public:
-   void nextPermutation(vector<int> &nums)
-{
-    // initialize variable:
-    int breakPoint = -1;
-
-    // find a breakpoint:
-    for (int i = nums.size() - 1; i > 0; i--)
-    {
-        if (nums[i] > nums[i - 1])
-        {
-            breakPoint = i - 1;
-            break;
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int ind=-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i+1]>nums[i]){
+                ind=i;
+                break;
+            }
         }
-    }
-
-    // if no breakpoint
-    if (breakPoint < 0)
-    {
-        reverse(nums.begin(), nums.end());
-        return;
-    }
-
-    // if found a breakpoint
-    for (int i = nums.size() - 1; i >= 0; i--)
-    {
-        if (nums[i] > nums[breakPoint])
-        {
-            swap(nums[breakPoint], nums[i]);
-            reverse(nums.begin() + breakPoint + 1, nums.end());
-            break;
+        if(ind==-1){
+            sort(nums.begin(),nums.end());
+            return ;
         }
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]>nums[ind]){
+                swap(nums[i],nums[ind]);
+                reverse(nums.begin()+ind+1,nums.end());
+                break;
+            }
+        }
+        
     }
-}
 };
