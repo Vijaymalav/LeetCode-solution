9a273fd0f1be3cd08d5a6c59b5b33e6d7cf40137
Vijay malav <91518113+Vijaymalav@users.noreply.github.com>
2022-06-08 01:50:01 +0530

    Added solution - LeetHub

--- Nearly sorted - GFG/nearly-sorted.cpp
+++ Nearly sorted - GFG/nearly-sorted.cpp
@@ -8,18 +8,39 @@ class Solution
 {
     public:
     //Function to return the sorted array.
-   vector <int> nearlySorted(int arr[], int num, int K){
-      priority_queue<int>p; vector<int>ans;
-      for(int i=0; i<num; i++) {
-          p.push(arr[i]);
-      }
-      while(!p.empty()) {
-          ans.push_back(p.top());
-          p.pop();
-      }
-      reverse(ans.begin(),ans.end());
-      return ans;
-   }
+//   vector <int> nearlySorted(int arr[], int num, int K){
+//       priority_queue<int>p; vector<int>ans;
+//       for(int i=0; i<num; i++) {
+//           p.push(arr[i]);
+//       }
+//       while(!p.empty()) {
+//           ans.push_back(p.top());
+//           p.pop();
+//       }
+//       reverse(ans.begin(),ans.end());
+//       return ans;
+//   }
+vector <int> nearlySorted(int arr[], int n, int k){
+        priority_queue <int, vector<int>, greater<int> > pq;
+        vector<int> res;
+        int val=0;
+        if(2*k>=n)
+            val=n-1;
+        else
+            val=2*k;
+        for(int i=0; i<=val; i++)
+            pq.push(arr[i]);
+        for(int i=val+1; i<n; i++) {
+            res.push_back(pq.top());
+            pq.pop();
+            pq.push(arr[i]);
+        }
+       while(!pq.empty()){
+            res.push_back(pq.top());
+            pq.pop();
+        }
+        return res;
+    }
 };
 //  0 1 2 3 4  5 6
 //  3 1 4 2 5
