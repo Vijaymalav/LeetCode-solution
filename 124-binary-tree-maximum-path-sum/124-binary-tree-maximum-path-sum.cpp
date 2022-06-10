6b3c7b890585531e6fa3291d69eae614e092143f
Vijay malav <91518113+Vijaymalav@users.noreply.github.com>
2022-06-10 13:15:46 +0530

    Time: 35 ms (54.55%), Space: 27.7 MB (68.77%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -11,33 +11,31 @@
  */
 class Solution {
 public:
-    // int mx=INT_MIN;
-    // int solve(TreeNode* root){
-    //       if(!root)return NULL;
-    //     int left=max(solve(root->left),0);
-    //     int right=max(solve(root->right),0);
-    //     if((left+right)<root->val){
-    //         mx=max(mx,root->val);
-    //     }
-    //     else mx=max(mx,left+right+root->val);
-    //     return max(left,right)+root->val;
-    // }
-    // int maxPathSum(TreeNode* root) {
-    //   solve(root);
-    //     return mx;
-    // }
-      int max_sum=INT_MIN;
-    int solve(TreeNode* root)
-    {
-        if(!root)return 0;
-        int l=max(solve(root->left),0);
-        int r=max(solve(root->right),0);
+    int mx=INT_MIN;
+    int solve(TreeNode* root){
+          if(!root)return NULL;
+        int left=max(solve(root->left),0);
+        int right=max(solve(root->right),0);
      
-        max_sum=max(max_sum,(root->val+l+r));
-        return root->val+max(l,r);
+       mx=max(mx,left+right+root->val);
+        return max(left,right)+root->val;
     }
     int maxPathSum(TreeNode* root) {
-        solve(root);
-        return max_sum;
+      solve(root);
+        return mx;
     }
+//       int max_sum=INT_MIN;
+//     int solve(TreeNode* root)
+//     {
+//         if(!root)return 0;
+//         int l=max(solve(root->left),0);
+//         int r=max(solve(root->right),0);
+     
+//         max_sum=max(max_sum,(root->val+l+r));
+//         return root->val+max(l,r);
+//     }
+//     int maxPathSum(TreeNode* root) {
+//         solve(root);
+//         return max_sum;
+//     }
 };
