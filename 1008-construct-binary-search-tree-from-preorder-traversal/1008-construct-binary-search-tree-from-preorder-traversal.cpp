e127b69aaf854e497f2fb0c66ba04b488d56f37f
Vijay malav <91518113+Vijaymalav@users.noreply.github.com>
2022-06-11 23:23:43 +0530

    Time: 7 ms (62.76%), Space: 22 MB (6.48%) - LeetHub

---
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ind=0;
+    int find(vector<int>vec,int st,int end,int val){
+        if(st>end)return -1;
+        for(int i=st;i<=end;i++){
+            if(vec[i]==val)return i;
+        }
+        return -1;
+    }
+    TreeNode* buildTree(vector<int>pre,vector<int>ino ,int st,int end){
+         if(st>end)return NULL;
+        int val=pre[ind];
+        ind++;
+        TreeNode* root=new TreeNode(val);
+      int mid=find(ino,st,end,val);
+        if(ind==-1)return NULL;
+        root->left=buildTree(pre,ino,st,mid-1);
+        root->right=buildTree(pre,ino,mid+1,end);
+        return root;
+        
+    }
+    TreeNode* bstFromPreorder(vector<int>& pre) {
+     vector<int>ino=pre;
+        sort(ino.begin(),ino.end());
+        return buildTree(pre,ino,0,pre.size()-1);
+    }
+};
