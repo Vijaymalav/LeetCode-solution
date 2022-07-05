748e365f738518f0d5c698ac6f730650cf608da4
Vijay malav <91518113+Vijaymalav@users.noreply.github.com>
2022-07-05 13:37:11 +0530

    Time: 220 ms (61.20%), Space: 49.8 MB (52.56%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -2,21 +2,39 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         if(nums.size()<=1)return nums.size();
-        sort(nums.begin(),nums.end());
-        int ele=nums[0],cnt=1,mx=INT_MIN;
-        for(int i=1;i<nums.size();i++){
-            if(ele==nums[i])continue;
-            if((ele+1)==nums[i]){
-                cnt++;
-                ele=nums[i];
-            }
-            else {
+        // sort(nums.begin(),nums.end());
+        // int ele=nums[0],cnt=1,mx=INT_MIN;
+        // for(int i=1;i<nums.size();i++){
+        //     if(ele==nums[i])continue;
+        //     if((ele+1)==nums[i]){
+        //         cnt++;
+        //         ele=nums[i];
+        //     }
+        //     else {
+        //         mx=max(cnt,mx);
+        //         ele=nums[i];
+        //         cnt=1;
+        //     }
+        // }
+        //  mx=max(cnt,mx);
+        // return mx;
+        unordered_map<int,bool>m;
+        for(auto it:nums)m[it]=true;
+        for(auto it:nums){
+            if(m.find(it-1)!=m.end())m[it]=false;
+        }
+        int mx=INT_MIN;
+        for(auto it:m){
+            if(it.second==true){
+              int  ele=it.first;
+               int cnt=1;
+                while(m.find(ele+1)!=m.end()){
+                    cnt++;
+                    ele++;
+                }
                 mx=max(cnt,mx);
-                ele=nums[i];
-                cnt=1;
             }
         }
-         mx=max(cnt,mx);
         return mx;
     }
 };
