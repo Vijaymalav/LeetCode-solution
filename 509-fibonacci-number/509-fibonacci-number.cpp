2d3b219094ad45081dd089384e188f033afd2827
Vijay malav <91518113+Vijaymalav@users.noreply.github.com>
2022-07-06 11:14:56 +0530

    Time: 5 ms (45.27%), Space: 5.9 MB (80.05%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
+     
         if(n==0 || n==1)return n;
-        return fib(n-1)+fib(n-2);
+        int f=0,s=1,val;
+        for(int i=1;i<n;i++){
+            val=f+s;
+            f=s;
+            s=val;
+        }
+        return val;
     }
 };
