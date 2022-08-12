08535a3ba3c9bbd2c96d4bf87f0f5afd83d23135
Vijay malav <91518113+Vijaymalav@users.noreply.github.com>
2022-08-12 19:37:22 +0530

    Time: 7 ms (99.72%), Space: 13.2 MB (52.88%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,33 +1,23 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& mat) {
+        unordered_set<int>row,col;
         int n=mat.size();
-        if(n==0)return;
         int m=mat[0].size();
-        unordered_set<int>Rst,Cst;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
-                    Rst.insert(i);
-                    Cst.insert(j);
+                    row.insert(i);
+                    col.insert(j);
                 }
-
-
             }
         }
-          for(int i=0;i<n;i++){
+            for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                     if(Rst.find(i)!=Rst.end()){
+                if(row.find(i)!=row.end() or col.find(j)!=col.end()){
                     mat[i][j]=0;
                 }
-                else if(Cst.find(j)!=Cst.end()){
-                     mat[i][j]=0;
-                }
-
             }
         }
-        
-        
-               
     }
 };
